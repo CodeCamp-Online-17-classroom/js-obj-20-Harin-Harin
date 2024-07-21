1fa384c0c12fac770bd7b3f49fe87a279895a081
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-22 00:09:38 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// write code here
+let calculator = {
+  
+  read() {
+      a = +prompt("Enter a number:");
+      b = +prompt("Enter b number:");
+  },
+
+  sum() {
+      return a + b;
+  },
+
+  mul() {
+      return a * b;
+  }
+
+};
+
+calculator.read();
+alert("Sum: " + calculator.sum());
+alert("Mul: " + calculator.mul());
